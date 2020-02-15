0daba2b368fea7c5d7ea0d4d307a1ccd8951ac61
Jackie <chowdhury1478@gmail.com>
2020-02-15 15:11:51 -0500

    required portion done

--- app/src/main/java/com/mystudyapps/studyme/MainActivity.java
+++ app/src/main/java/com/mystudyapps/studyme/MainActivity.java
@@ -3,13 +3,45 @@ package com.mystudyapps.studyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView questionTV, answerTV;
+
+
+
+
+    public void questionOnClick(View view){
+        questionTV.setEnabled(false);
+        questionTV.setVisibility(View.INVISIBLE);
+
+        answerTV.setVisibility(View.VISIBLE);
+        answerTV.setEnabled(true);
+
+    }
+
+
+    public void answerOnClick(View view){
+        answerTV.setVisibility(View.INVISIBLE);
+        answerTV.setEnabled(false);
+
+        questionTV.setEnabled(true);
+        questionTV.setVisibility(View.VISIBLE);
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        questionTV = findViewById(R.id.questionTV);
+        answerTV = findViewById(R.id.answerTV);
+
+        answerTV.setVisibility(View.INVISIBLE);
+        answerTV.setEnabled(false);
     }
 }
