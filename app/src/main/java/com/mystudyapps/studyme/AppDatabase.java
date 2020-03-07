45d97ae40a60c1f0922db63d42581e0f1079b640
Jackie <chowdhury1478@gmail.com>
2020-03-07 18:49:15 -0500

    fixed build.grade(:app) bug +  Created the database related files (ie. appDatabase.java, Flashcard.java,FlashcardDao.java, FlashcardDatabase.java)

---
+++ app/src/main/java/com/mystudyapps/studyme/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.mystudyapps.studyme;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Flashcard.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract FlashcardDao flashcardDao();
+}
